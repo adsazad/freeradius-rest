40b0d274e3179f6542df4fd1a7a887f15ed71476
Arashdeep Singh <singh@arashdeep.co.in>
2022-04-02 13:33:51 +0530

    fix

--- routes/nas.js
+++ routes/nas.js
@@ -27,7 +27,7 @@ router.post('/create', async function (req, res) {
     var adminuser = await auth.chAuth(req, res);
     var con = db.getConnect();
     var nasname = req.body.nasname;
-    var secret = req.body.nassecret;
+    var nassecret = req.body.nassecret;
     var shortname = req.body.shortname;
     var type = req.body.type;
     var ports = req.body.ports;
