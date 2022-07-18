40288ed25f6d4126db6916ff49139c80615d7c12
Arashdeep Singh <singh@arashdeep.co.in>
2022-07-18 11:54:06 +0530

    new user creation fix.

--- routes/users.js
+++ routes/users.js
@@ -38,47 +38,15 @@ router.post("/create", async function (req, res, next) {
   var cusername = req.body.radusername;
   var cpassword = req.body.radpassword;
   con.connect(function (err) {
-    var checkUserUser = "SELECT * FROM userinfo WHERE username = '" + cusername + "'";
-    con.query(checkUserUser, function (err, result) {
-      if (result.length == 0) {
-        //  create freeradius user sql
-        var insq = "INSERT INTO `userinfo` (`username`, `firstname`, `lastname`, `email`, `department`, `company`, `workphone`, `homephone`, `mobilephone`, `address`, `city`, `state`, `country`, `zip`, `notes`, `changeuserinfo`, `portalloginpassword`, `enableportallogin`, `creationdate`, `creationby`, `updatedate`, `updateby`) VALUES ('" + cusername + "', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0,NOW(), '" + adminuser.username + "', NOW(), '" + adminuser.username + "')";
-        console.log(insq);
-        con.query(insq, function (err, inresult) {
-          if (err) {
-            console.log(err);
-            res.json({
-              status: "errpr",
-              message: "Error creating user: " + err
-            });
-            res.end();
-          }
-          if (inresult) {
-            console.log("User created successfully");
-            radius.addUserCheckAttribute(cusername, "Cleartext-Password", ":=", cpassword);
-            res.json({
-              status: "success",
-              message: "User created successfully",
-              data: inresult,
-            });
-            res.end();
-          } else {
-            console.log("Error creating user");
-            res.json({
-              status: "error",
-              message: "Error creating user"
-            });
-            res.end();
-          }
-        });
-      } else {
-        res.json({
-          "status": "error",
-          "message": "Username already exists"
-        });
-        res.end();
-      }
-    });
+    //  create freeradius user sql
+      console.log("User created successfully");
+      radius.addUserCheckAttribute(cusername, "Cleartext-Password", ":=", cpassword);
+      res.json({
+        status: "success",
+        message: "User created successfully",
+        data: inresult,
+      });
+      res.end();
   });
 });
 
